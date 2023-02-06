c04b5389e058c82051308fa0a838a0a26f905528
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-02-06 11:04:08 +0700

    implemented reusable utility

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,20 +1,37 @@
-type RGB = 'red' | 'green' | 'blue';
-interface IProps<C extends React.ElementType> {
+type RGB = "red" | "green" | "blue";
+
+type AsProp<C extends React.ElementType> = {
   as?: C;
-  color?: RGB | 'black';
 };
+
+// type TextProps<C extends React.ElementType> = AsProp<C> & {
+//   color?: RGB | "black";
+// };
 
 // Create a robust polymorphic component with its own props
 // Omit an element if its available by default and use default
-type Props<C extends React.ElementType> = React.PropsWithChildren<
-  IProps<C> & Omit<React.ComponentPropsWithoutRef<C>, keyof IProps<C>>
->;
+// type Props<C extends React.ElementType> = React.PropsWithChildren<
+//   TextProps<C>
+// > &
+//   Omit<React.ComponentPropsWithoutRef<C>, keyof TextProps<C>>;
 
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = {}
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+  type TextProps = {
+    color?: RGB | "black";
+  }
 const Text = <C extends React.ElementType = "span">({
   as,
+  style,
   children,
   ...restProps
-}: Props<C>) => {
+}: PolymorphicComponentProps<C, TextProps>) => {
   const Component = as || "span";
   return <Component {...restProps}>{children}</Component>;
 };
